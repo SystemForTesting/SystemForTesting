54bde465bfe85e4b91a610ab4dee02d2b71c5b9e
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-06-09 11:11:21 +0600

    added wildfly,  hibernate and EntityManager bean to project

--- src/main/java/com/epam/testsystem/model/TestCase.java
+++ src/main/java/com/epam/testsystem/model/TestCase.java
@@ -2,10 +2,9 @@ package com.epam.testsystem.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 public class TestCase extends BaseEntity {
     private LocalDateTime startedAt;
     private Test test;
-    private Map<Question, List<Answer>> answers;
+    private List<Answer> answers;
 }
