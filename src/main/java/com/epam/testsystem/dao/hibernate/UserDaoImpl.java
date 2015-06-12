e7b17e219e3ce92a826450e8b9209b4eb7e2cb6a
Logrus86 <logr86@gmail.com>
2015-06-12 17:57:19 +0600

    initial setup with Spring Security

--- src/main/java/com/epam/testsystem/dao/hibernate/UserDaoImpl.java
+++ src/main/java/com/epam/testsystem/dao/hibernate/UserDaoImpl.java
@@ -2,11 +2,16 @@ package com.epam.testsystem.dao.hibernate;
 
 import com.epam.testsystem.dao.UserDao;
 import com.epam.testsystem.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
 public class UserDaoImpl extends AbstractDao<User> implements UserDao {
+    private static final Logger LOG= LoggerFactory.getLogger(UserDaoImpl.class);
+
     public UserDaoImpl(EntityManager em) {
         super(em);
     }
@@ -15,7 +20,25 @@ public class UserDaoImpl extends AbstractDao<User> implements UserDao {
     public User findByCredentials(User user) {
         TypedQuery<User> query = em.createQuery("select u from User u where lower(u.username) = " +
                 "lower('" + user.getUsername() + "') and u.password = '" + user.getPassword() + "'", User.class);
-        User result = query.getSingleResult();
-        return result;
+        try {
+            User result = query.getSingleResult();
+            return result;
+        } catch(NoResultException e) {
+            LOG.warn("Could not find user with username {} and password {}", user.getUsername(), user.getPassword(), e);
+        }
+        return null;
+    }
+
+    @Override
+    public User findByName(String username) {
+        TypedQuery<User> query = em.createQuery("select u from User u where lower(u.username) = " +
+                "lower('" + username + "')", User.class);
+        try {
+            User result = query.getSingleResult();
+            return result;
+        } catch(NoResultException e) {
+            LOG.warn("Could not find user with username {}", username, e);
+        }
+        return null;
     }
 }
