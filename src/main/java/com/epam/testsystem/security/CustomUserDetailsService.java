53746d7f81ac51f0cc2d2afe1ba55625fc06d031
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-06-15 17:04:42 +0600

    added entityManager to springContainer and invoked in CustomUserDetailsService

--- src/main/java/com/epam/testsystem/security/CustomUserDetailsService.java
+++ src/main/java/com/epam/testsystem/security/CustomUserDetailsService.java
@@ -10,12 +10,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-@Component
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
