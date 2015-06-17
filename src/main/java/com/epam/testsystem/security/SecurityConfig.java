149aa94aeec95423c398036c768674910f6811db
Logrus86 <logr86@gmail.com>
2015-06-17 13:01:04 +0600

    added SecurityUser, moved security related stuff to its own package and fixed antMatchers

--- src/main/java/com/epam/testsystem/security/SecurityConfig.java
+++ src/main/java/com/epam/testsystem/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,16 +26,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/static/**");
+    }
+
+    @Override
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/welcome.do").hasRole("ADMIN")
-                    .and()
+                    .antMatchers("/login", "/login.do").anonymous()
+                .antMatchers("/admin", "/admin**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
                     .loginPage("/login.do")
                     .loginProcessingUrl("/login")
                     .failureUrl("/login?error=1")
-                    .permitAll()
                     .and()
                 .csrf();
     }
--- src/main/java/com/epam/testsystem/security/SecurityUser.java
+++ src/main/java/com/epam/testsystem/security/SecurityUser.java
@@ -1,4 +1,57 @@
 package com.epam.testsystem.security;
 
-public class SecurityUser {
+import com.epam.testsystem.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SecurityUser extends User implements UserDetails {
+    public SecurityUser(User user) {
+        if(user != null) {
+            this.setId(user.getId());
+            this.setDeleted(user.isDeleted());
+            this.setCreatedAt(user.getCreatedAt());
+            this.setUpdatedAt(user.getUpdatedAt());
+            this.setUsername(user.getUsername());
+            this.setEmail(user.getEmail());
+            this.setFirstName(user.getFirstName());
+            this.setLastName(user.getLastName());
+            this.setPassword(user.getPassword());
+            this.setRole(user.getRole());
+            this.setTestCases(user.getTestCases());
+        }
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authSet = new HashSet<>();
+        authSet.add(new SimpleGrantedAuthority("ROLE_" + this.getRoleName().toUpperCase()));
+        return authSet;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 }
