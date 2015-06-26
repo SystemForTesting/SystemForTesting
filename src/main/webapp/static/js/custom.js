fcf6ad2eea9b25bb7926d7189619e10d739ed937
Admin <Rody10@mail.ru>
2015-06-26 17:09:37 +0600

    Fixed creating test page.

--- src/main/webapp/static/js/custom.js
+++ src/main/webapp/static/js/custom.js
@@ -19,4 +19,6 @@ function removeAnswer(element){
     $('a').last().focus();
 }
 
-//'<div class="question-form"><label>Вопрос №</label><div><textarea class="form-control" rows="3" style="margin: 0px -7px 0px 0px; width: 914px; height: 80px;"></textarea></div><div class="answers"><div class="answer"><div><div class="input-group"><span class="input-group-addon"><span class="glyphicon glyphicon-remove" onclick="removeAnswer(this)" aria-hidden="true"></span></span><textarea class="form-control" rows="3" style="margin: 0px; width: 837px; height: 74px;"></textarea><span class="input-group-addon"><input type="checkbox" aria-label="..."></span></div></div></div></div><a onclick="addAnswer(this)" href="javascript:void(0)" class="btn btn-primary btn-raised add-ans-button">Add answer</a><div class="clearfix"></div></div>'
+//function(){
+//
+//}
