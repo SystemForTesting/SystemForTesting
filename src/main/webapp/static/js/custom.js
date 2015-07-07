4c501fbc99899a73e50c2e4022e7f9ff9e9b9cd0
yakovshmidt <yakovshmidt93@gmail.com>
2015-07-07 16:44:17 +0600

    edited custom.js

--- src/main/webapp/static/js/custom.js
+++ src/main/webapp/static/js/custom.js
@@ -3,7 +3,10 @@
 $('.main-quest-panel').on('click', '.add-qwest-button', function () {
     var nextQuestionId = $('.question-form').last().attr('id') - 1 + 2;
     var nextQuestionNumber = $('.question-form').last().attr('id') - 1 + 3;
-
+    if (isNaN(nextQuestionNumber)) {
+        nextQuestionId = 0;
+        nextQuestionNumber = 1;
+    }
     $('.questions').append('<div class="question-form" id="' + nextQuestionId + '"><label>' + 'Вопрос №' + nextQuestionNumber + '</label><div><textarea name="questions[' + nextQuestionId + '].title" rows="3" class="form-control vresize"></textarea></div><div class="answers"> <div class="answer"><div><div class="input-group"><span class="input-group-addon"><span class="glyphicon glyphicon-remove" onclick="removeAnswer(this)" aria-hidden="true"></span></span><textarea name="questions[' + nextQuestionId + '].answers[0].text" rows="3" class="form-control vresize"></textarea><span class="input-group-addon"><input type="checkbox" name="questions[' + nextQuestionId + '].answers[0].right"> </span></div></div></div></div><a onclick="addAnswer(this)" href="javascript:void(0)" class="btn btn-primary btn-raised add-ans-button">Add answer</a> <div class="clearfix"></div></div>');
     $('textarea').last().focus();
 });
@@ -24,15 +27,16 @@ function removeAnswer(element) {
 
 //------------------------------START USER PAGE------------------------------------------------
 
-$(document).keydown(function(e) {
-    switch(e.which) {
+$(document).keydown(function (e) {
+    switch (e.which) {
         case 37: // left
             switchQuestionToPrevious();
             break;
         case 39: // right
             switchQuestionToNext();
             break;
-        default: return; // exit this handler for other keys
+        default:
+            return; // exit this handler for other keys
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
@@ -99,7 +103,7 @@ $('.onoffswitch').on('click', 'input', function () {
 
 });
 
-function getButtonChar(element, currentIndex){
+function getButtonChar(element, currentIndex) {
     var firsChar = "A".charCodeAt(0) - 1;
     var nextChar = String.fromCharCode(firsChar + currentIndex);
     $(element).text(nextChar)
@@ -112,11 +116,11 @@ function selectAnswer(element) {
     $(".question-button.current").addClass('answered');
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     var allSpans = $('.custom-btn-block label span');
     var firsChar = "A".charCodeAt(0);
 
-    $.each(allSpans, function(index, value) {
+    $.each(allSpans, function (index, value) {
         var currentIndex = $(value).attr('current-index');
         var summ = parseInt(firsChar, 10) + parseInt(currentIndex, 10);
         var nextChar = String.fromCharCode(summ);
