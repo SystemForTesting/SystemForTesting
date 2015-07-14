1d91a82b2e4a7690a78a16d8340bf006c3d2733e
yakovshmidt <hatch-abaddon@mail.ru>
2015-07-14 23:39:51 +0600

    JavaMail works

---
+++ src/main/java/com/epam/testsystem/main/MailMain.java
@@ -0,0 +1,51 @@
+package com.epam.testsystem.main;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailMain {
+
+    public static void main(String[] args) {
+
+        final String username = "systemfortesting2015@gmail.com";
+        final String password = "systemfortesting";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("systemfortesting2015@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("yakovshmidt93@gmail.com"));
+            message.setSubject("Theme of message");
+            message.setText("Dear Mail,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
